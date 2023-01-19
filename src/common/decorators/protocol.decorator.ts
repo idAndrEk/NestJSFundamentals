31dfc9167dd19c6464dc3f2bdb3fe7de8d04ebb5
Andrey <AndrEkat96@yandex.ru>
2023-01-19 17:34:45 +0500

    Other Building Blocks by Example

--- src/common/decorators/protocol.decorator.ts
+++ src/common/decorators/protocol.decorator.ts
@@ -3,6 +3,7 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 export const Protocol = createParamDecorator(
   (defaultValue: string, ctx: ExecutionContext) => {
     console.log({defaultValue});
+    console.log('123');
     const request = ctx.switchToHttp().getRequest();
     return request.protocol;
   }
