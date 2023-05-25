178de87e8ab4c9565a8c7b298230fae858c5fa96
Andrey <AndrEkat96@yandex.ru>
2023-05-25 10:58:42 +0500

    connection MongoDb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CoffeesController } from './coffees/coffees.controller';
-import { CoffeesService } from './coffees/coffees.service';
-import { CoffeesModule } from './coffees/coffees.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { CoffeesController } from "./coffees/coffees.controller";
+import { CoffeesService } from "./coffees/coffees.service";
+import { CoffeesModule } from "./coffees/coffees.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot("mongodb+srv://AndreyId:23717193@cluster0.trmpa.mongodb.net/?retryWrites=true&w=majority")
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
